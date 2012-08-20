5136be25daffec998b58427e8793e40556531215
djhworld <djharperuk@gmail.com>
2012-08-20 21:46:47 +0100

    Re-organised benchmarks to be in correct order

--- benchmarks_test.go
+++ benchmarks_test.go
@@ -4,45 +4,10 @@ import "testing"
 
 var inputStr string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-func BenchmarkHead(b *testing.B) {
+func BenchmarkAll(b *testing.B) {
+	var isLower func(rune) bool = func(r rune) bool { return r >= 97 && r <= 122 }
 	for i := 0; i < b.N; i++ {
-		Head(inputStr)
-	}
-}
-
-func BenchmarkTail(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		Tail(inputStr)
-	}
-}
-
-func BenchmarkInit(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		Init(inputStr)
-	}
-}
-
-func BenchmarkTake(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		Take(12, inputStr)
-	}
-}
-
-func BenchmarkLast(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		Last(inputStr)
-	}
-}
-
-func BenchmarkDrop(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		Drop(12, inputStr)
-	}
-}
-
-func BenchmarkReverse(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		Reverse(inputStr)
+		All(isLower, inputStr)
 	}
 }
 
@@ -52,31 +17,9 @@ func BenchmarkDistinct(b *testing.B) {
 	}
 }
 
-func BenchmarkGroup(b *testing.B) {
+func BenchmarkDrop(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Group(inputStr)
-	}
-}
-
-func BenchmarkFilter(b *testing.B) {
-	var isLower func(rune) bool = func(r rune) bool { return r >= 97 && r <= 122 }
-	for i := 0; i < b.N; i++ {
-		Filter(isLower, inputStr)
-	}
-}
-
-func BenchmarkAll(b *testing.B) {
-	var isLower func(rune) bool = func(r rune) bool { return r >= 97 && r <= 122 }
-	for i := 0; i < b.N; i++ {
-		All(isLower, inputStr)
-	}
-}
-
-func BenchmarkTakeWhile(b *testing.B) {
-	var isLower func(rune) bool = func(r rune) bool { return r >= 97 && r <= 122 }
-
-	for i := 0; i < b.N; i++ {
-		TakeWhile(isLower, inputStr)
+		Drop(12, inputStr)
 	}
 }
 
@@ -88,6 +31,49 @@ func BenchmarkDropWhile(b *testing.B) {
 	}
 }
 
+func BenchmarkFilter(b *testing.B) {
+	var isLower func(rune) bool = func(r rune) bool { return r >= 97 && r <= 122 }
+	for i := 0; i < b.N; i++ {
+		Filter(isLower, inputStr)
+	}
+}
+
+func BenchmarkGroup(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Group(inputStr)
+	}
+}
+
+func BenchmarkHead(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Head(inputStr)
+	}
+}
+
+func BenchmarkInit(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Init(inputStr)
+	}
+}
+
+func BenchmarkIsEmpty(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		IsEmpty(inputStr)
+	}
+}
+
+func BenchmarkLast(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Last(inputStr)
+	}
+}
+
+func BenchmarkReverse(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Reverse(inputStr)
+	}
+}
+
 func BenchmarkSpan(b *testing.B) {
 	var isLower func(rune) bool = func(r rune) bool { return r >= 97 && r <= 122 }
 
@@ -96,3 +82,22 @@ func BenchmarkSpan(b *testing.B) {
 	}
 }
 
+func BenchmarkTail(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Tail(inputStr)
+	}
+}
+
+func BenchmarkTake(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Take(12, inputStr)
+	}
+}
+
+func BenchmarkTakeWhile(b *testing.B) {
+	var isLower func(rune) bool = func(r rune) bool { return r >= 97 && r <= 122 }
+
+	for i := 0; i < b.N; i++ {
+		TakeWhile(isLower, inputStr)
+	}
+}
